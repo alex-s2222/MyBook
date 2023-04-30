fa7ea7035232a46e80f22986781e17475dba5b15
Juzo(-.-) <alexs22221@gmail.com>
2023-04-30 18:23:00 +0300

    Очереди Multiprocessing

--- Python/SimplePython/Multiprocessing/dishes.py
+++ Python/SimplePython/Multiprocessing/dishes.py
@@ -15,18 +15,17 @@ def dryer(input):
     while True:
         dish = input.get()
         print('Drying', dish,'dish')
-        # sleep(randint(0,10)) 
+        sleep(randint(0,10)) 
         input.task_done()
 
 if __name__ == '__main__':
     dish_queue = mp.JoinableQueue() #создаем очередь
     dryer_proc = mp.Process(target=dryer, args=(dish_queue,)) #создаем процесс 
-
     dryer_proc.daemon = True #убивает процесс когда заканчивается очередь
     dryer_proc.start() #запускаем поток на получение задачи
 
     dishes = ['salad', 'bread', 'entree', 'dessert']
     washer(dishes, dish_queue) # заносим в очередь 
-    dish_queue.join() # объединяем очередь 
+    dish_queue.join() # блокируем пока не обработаются все задачи
 
 
